23bcd5119ad37d5a8783a9a15be330dec1855c2f
GALVAN BARZOLA <manuel.galvan.27@unsch.edu.pe>
2023-02-07 15:59:25 -0500

    por favor

--- Router.php
+++ Router.php
@@ -16,11 +16,7 @@ class Router
     }
 
     public function comprobarRutas() {
-        if ($_SERVER['PATH_INFO']) {
-            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
-        } else {
-            $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
-        }
+        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'] ;
         $method = $_SERVER['REQUEST_METHOD'];
             
         //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
