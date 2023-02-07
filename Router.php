17295a2c35bd41c5941ed8c2ed36f19fcfbe8e12
GALVAN BARZOLA <manuel.galvan.27@unsch.edu.pe>
2023-02-07 16:40:06 -0500

    copiando git

--- Router.php
+++ Router.php
@@ -22,15 +22,11 @@ class Router
             $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         }
         $method = $_SERVER['REQUEST_METHOD'];
-            
-        //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
-        $splitURL = explode('?', $currentUrl);
-        // debuguear($splitURL);
-        
+
         if ($method === 'GET') {
-            $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
+            $fn = $this->getRoutes[$currentUrl] ?? null;
         } else {
-        $fn = $this->postRoutes[$splitURL[0]] ?? null;
+            $fn = $this->postRoutes[$currentUrl] ?? null;
         }
 
         if ( $fn ) {
